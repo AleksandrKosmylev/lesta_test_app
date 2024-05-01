ec0f6cc59ddc7c9c40ff7c92483fde9bdab59ada
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-05-01 16:24:58 +0300

    add upload file option

--- analyzer/urls.py
+++ analyzer/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from analyzer.views import index
+from django.conf.urls.static import static
+from django.conf import settings
+# from analyzer.views import IndexView
 
 
 urlpatterns = [
     path('', index)
+    # path('', IndexView.as_view())
 ]
 
-
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- analyzer/views.py
+++ analyzer/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.core.files.storage import FileSystemStorage
 from django.http import HttpResponse
+from django.views.generic import ListView
+
 
 def index(request):
-    return HttpResponse("START")
+    # return HttpResponse("START")
     # return render(request, 'index.html', {'workers': Employees.objects.all()})
+    context = {}
+    if request.method == 'POST':
+        uploaded_file = request.FILES['document']
+        fs = FileSystemStorage()
+        name = fs.save(uploaded_file.name, uploaded_file)
+        context['url'] = fs.url(name)
+        print(uploaded_file.name)
+    return render(request, 'upload.html', context)
+
+
+"""
+class IndexView(ListView):
+    template_name = 'index.html'
+"""
