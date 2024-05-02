ab368076f0b00c409f668f9a9ed015c62765214f
AleksandrKosmylev <alex.kosmylev@gmail.com>
2024-05-02 21:14:07 +0300

    fix table of results

--- analyzer/models.py
+++ analyzer/models.py
@@ -4,4 +4,7 @@ from django.db import models
 class Word(models.Model):
     name = models.CharField(max_length=255, verbose_name='name')
     tf = models.IntegerField(verbose_name='tf')
-    idf = models.IntegerField(verbose_name='idf')
+    idf = models.FloatField(verbose_name='idf')
+
+    def __str__(self):
+        return self.name
